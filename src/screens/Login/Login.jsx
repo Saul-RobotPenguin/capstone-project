a58649abb57b7036b81cf244ae26d9d5e24de84d
Josue Ramirez-Romero <josueramirez35827@gmail.com>
2022-06-22 16:54:27 -0400

    Updated Forms

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -53,28 +53,32 @@ const Login = () => {
   return (
     <>
       {error && <p>{status}</p>}
+    
 
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <input
-          type="text"
-          name="username"
-          placeholder="Your Username"
-          class="input input-bordered w-full max-w-xs"
-          onChange={(e) => setUsersUsername(e.target.value)}
-        />
+    <div class="w-screen h-screen flex justify-center items-center
+    bg-gradient-to-br from-purple-700 to-amber-700">
+        <form class="p-10 bg-white rounded-xl drop-shadow-lg space-y-5" onSubmit={(e) => handleSubmit(e)} >
+            <h1 class="text-center text-3xl">LOGIN</h1>
+            <div class="flex flex-col space-y-2">
+                <label class="text-sm font-light" for="email">Username</label>
+                <input class="w-96 px-3 py-2 rounded-md border border-slate-400" type="text" placeholder="Your Username"
+                name="username" id="username" onChange={(e) => setUsersUsername(e.target.value)}
+              required/>
+          </div>
+          
+            <div class="flex flex-col space-y-2">
+                <label class="text-sm font-light" for="password">Password</label>
+                <input class="w-96 px-3 py-2 rounded-md border border-slate-400" type="password"
+                placeholder="Your Password" name="password" id="password" onChange={(e) => setUsersPassword(e.target.value)}
+              required/>
+            </div>
 
-        <input
-          type="password"
-          name="password"
-          placeholder="Your Password"
-          class="input input-bordered w-full max-w-xs"
-          onChange={(e) => setUsersPassword(e.target.value)}
-        />
+            <button class="btn" type="submit" id="submit">
+                Log In
+            </button>
+        </form>
+    </div>
 
-        <button id="submit" type="submit" className="btn">
-          Submit
-        </button>
-      </form>
     </>
   );
 };
